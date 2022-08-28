6fd4d2edb952127038b9b61b163c34b48dada714
Joe Stanley <engineerjoe440@yahoo.com>
2022-08-28 13:47:36 -0700

    carved out some more of the base-level syntax

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # schemdraw-markdown
 *a simpler way to document simple circuits in markdown*
 
-[schemdraw](https://bitbucket.org/cdelker/schemdraw/src/master/) in markdown files.
+[schemdraw](https://bitbucket.org/cdelker/schemdraw/src/master/) in markdown
+files.
+
+**Inspiration:** This project is inspired by the wonderful project:
+[`plantuml-markdown`](https://github.com/mikitex70/plantuml-markdown). So,
+shoutout to the folks who've toiled on that project to make it great!
+
+## Usage
+
+This package allows you to configure a schematic drawing, directly in markdown
+using a code-fenced sample as follows:
+
+```
+::schemdraw:: alt="My super diagram"
+    += elm.Resistor().right().label('1Ω')
+    += elm.Capacitor().down().label('10μF')
+    += elm.Line().left()
+    += elm.SourceSin().up().label('10V')
+::end-schemdraw::
+```
--- schemdraw-markdown.py
+++ schemdraw-markdown.py
@@ -3,22 +3,161 @@
 Schemdraw Markdown - A simpler way to document simple circuits in markdown.
 
 License: MIT
+
+This plugin implements a block extension which can be used to specify a
+Schematic Diagram which will be converted into an image and inserted in the
+document using [schemdraw](https://bitbucket.org/cdelker/schemdraw/src/master/).
+
+Syntax:
+
+    ::schemdraw:: [alt="text for alt"]
+        schemdraw script diagram
+    ::end-schemdraw::
+
+Example:
+
+    ::schemdraw:: alt="My super diagram"
+        += elm.Resistor().right().label('1Ω')
+        += elm.Capacitor().down().label('10μF')
+        += elm.Line().left()
+        += elm.SourceSin().up().label('10V')
+    ::end-schemdraw::
 """
 ################################################################################
 
 import re
+import base64
+import logging
 from xml.etree import ElementTree as etree
 
 import markdown
 
 
+# use markdown_py with -v to enable warnings, or with --noisy to enable debug logs
+logger = logging.getLogger('MARKDOWN')
+
+
 # For details see https://pythonhosted.org/Markdown/extensions/api.html#blockparser
 class SchemDrawPreprocessor(markdown.preprocessors.Preprocessor):
+    # Regular expression inspired from fenced_code
+    BLOCK_RE = re.compile(r'''
+        (?P<indent>[ ]*)
+        ::schemdraw:: 
+        # args
+        \s*(alt=(?P<quot2>"|')(?P<alt>.*?)(?P=quot2))?
+        \s*(title=(?P<quot3>"|')(?P<title>.*?)(?P=quot3))?
+        \s*(width=(?P<quot4>"|')(?P<width>[\w\s"']+%?)(?P=quot4))?
+        \s*(height=(?P<quot5>"|')(?P<height>[\w\s"']+%?)(?P=quot5))?
+        \s*(source=(?P<quot6>"|')(?P<source>.*?)(?P=quot6))?
+        \s*\n
+        (?P<code>.*?)(?<=\n)
+        (?P=indent)::end-schemdraw::[ ]*$
+        ''', re.MULTILINE | re.DOTALL | re.VERBOSE)
+
+    FENCED_BLOCK_RE = re.compile(r'''
+        (?P<indent>[ ]*)
+        (?P<fence>(?:~{3}|`{3}))[ ]*            # Opening ``` or ~~~
+        (\{?\.?schemdraw)[ ]*                   # Optional {, and lang
+        # args
+        \s*(alt=(?P<quot2>"|')(?P<alt>.*?)(?P=quot2))?
+        \s*(title=(?P<quot3>"|')(?P<title>.*?)(?P=quot3))?
+        \s*(width=(?P<quot4>"|')(?P<width>[\w\s"']+%?)(?P=quot4))?
+        \s*(height=(?P<quot5>"|')(?P<height>[\w\s"']+%?)(?P=quot5))?
+        \s*(source=(?P<quot6>"|')(?P<source>.*?)(?P=quot6))?
+        [ ]*
+        }?[ ]*\n                                # Optional closing }
+        (?P<code>.*?)(?<=\n)
+        (?P=indent)(?P=fence)[ ]*$
+        ''', re.MULTILINE | re.DOTALL | re.VERBOSE)
+    # (?P<indent>[ ]*)(?P<fence>(?:~{3}|`{3}))[ ]*(\{?\.?schemdraw)[ ]*\n(?P<code>.*?)(?<=\n)(?P=indent)(?P=fence)$
+    FENCED_CODE_RE = re.compile(r'(?P<fence>(?:~{4,}|`{4,})).*?(?P=fence)',
+                                re.MULTILINE | re.DOTALL | re.VERBOSE)
     """Schemdraw Preprocessor for Markdown"""
 
-    pass # TODO
+    def __init__(self, md):
+        super(SchemDrawPreprocessor, self).__init__(md)
 
+    def run(self, lines):
+        text = '\n'.join(lines)
+        idx = 0
 
+        # loop until all text is parsed
+        while idx < len(text):
+            text1, idx1 = self._replace_block(text[idx:])
+            text = text[:idx]+text1
+            idx += idx1
+
+        return text.split('\n')
+
+    def _replace_block(self, text):
+        # skip fenced code enclosing diagram
+        m = self.FENCED_CODE_RE.search(text)
+        if m:
+            # check if before the fenced code there is a plantuml diagram
+            m1 = self.FENCED_BLOCK_RE.search(text[:m.start()])
+            if m1 is None:
+                # no diagram, skip this block of text
+                return text, m.end()+1
+
+        # Parse configuration params
+        m = self.FENCED_BLOCK_RE.search(text)
+        if not m:
+            m = self.BLOCK_RE.search(text)
+            if not m:
+                return text, len(text)
+
+        # Parse configuration params
+        alt = m.group('alt') if m.group('alt') else self.config['alt']
+        title = m.group('title') if m.group('title') else self.config['title']
+        width = m.group('width') if m.group('width') else None
+        height = m.group('height') if m.group('height') else None
+        source = m.group('source') if m.group('source') else None
+
+        # Extract the PlantUML code.
+        code = ""
+        # Add extracted markdown diagram text.
+        code += m.group('code')
+
+        # Extract diagram source end convert it (if not external)
+        diagram = self._render_diagram(code)
+        self_closed = True  # tags are always self closing
+        map_tag = ''
+
+        # Firefox handles only base64 encoded SVGs
+        data = 'data:image/svg+xml;base64,{0}'.format(
+            base64.b64encode(diagram).decode('ascii')
+        )
+        img = etree.Element('img')
+        img.attrib['src'] = data
+
+        styles = []
+        if 'style' in img.attrib and img.attrib['style'] != '':
+            styles.append(re.sub(r';$', '', img.attrib['style']))
+        if width:
+            styles.append("max-width:"+width)
+        if height:
+            styles.append("max-height:"+height)
+
+        if styles:
+            img.attrib['style'] = ";".join(styles)
+            img.attrib['width'] = '100%'
+            if 'height' in img.attrib:
+                img.attrib.pop('height')
+
+        img.attrib['alt'] = alt
+        img.attrib['title'] = title
+
+        diag_tag = etree.tostring(img, short_empty_elements=self_closed).decode()
+        diag_tag = diag_tag + map_tag
+
+        return (
+            text[:m.start()] + m.group('indent') + diag_tag + text[m.end():],
+            m.start() + len(m.group('indent')) + len(diag_tag)
+        )
+
+    def _render_diagram(self, code):
+        """Render the Diagram"""
+        code = code.encode('utf8')
 
 
 # For details see https://pythonhosted.org/Markdown/extensions/api.html#extendmarkdown
